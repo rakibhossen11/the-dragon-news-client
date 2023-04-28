245622ae5e65f21e65f80324859a3abc40abd7ce
Rakib Hossen <rakibhossen1660@gmail.com>
2023-04-28 10:45:12 +0600

    news details

---
+++ src/provider/AuthProvider.jsx
@@ -0,0 +1,22 @@
+import React, { createContext } from 'react';
+import {getAuth} from 'firebase/auth';
+import app from '../firebase/firebase.config';
+
+export const AuthContext = createContext(null);
+const auth = getAuth(app);
+
+const AuthProvider = ({children}) => {
+
+    const user = {displayName: 'Muri khan'};
+    const auhtInfo = {
+        user
+    };
+
+    return (
+        <AuthContext.Provider value={auhtInfo}>
+            {children}
+        </AuthContext.Provider>
+    );
+};
+
+export default AuthProvider;
