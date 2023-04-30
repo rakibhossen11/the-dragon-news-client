2b9ae8dcff8834a40ff3214f89099aeb7c8edefe
Rakib Hossen <rakibhossen1660@gmail.com>
2023-04-30 12:10:43 +0600

    firebase deploy

--- src/pages/Home/NewsCard/NewsCard.jsx
+++ src/pages/Home/NewsCard/NewsCard.jsx
@@ -2,11 +2,18 @@ import moment from "moment/moment";
 import React from "react";
 import { Card, Image } from "react-bootstrap";
 import { Link } from "react-router-dom";
-import { FaEye, FaRegBookmark,FaRegStar,FaShareAlt, FaStar } from "react-icons/fa";
-import Rating from "react-rating";
+import {
+  FaEye,
+  FaRegBookmark,
+  FaRegStar,
+  FaShareAlt,
+  FaStar,
+} from "react-icons/fa";
+import { Rating } from "@smastrom/react-rating";
+import "@smastrom/react-rating/style.css";
 
 const NewsCard = ({ news }) => {
-  const { _id, title, details, image_url, author,total_view,rating } = news;
+  const { _id, title, details, image_url, author, total_view, rating } = news;
   return (
     <Card className="mb-4">
       <Card.Header className="d-flex align-items-center">
@@ -36,18 +43,14 @@ const NewsCard = ({ news }) => {
         </Card.Text>
       </Card.Body>
       <Card.Footer className="text-muted d-flex">
-        <div className="flex-grow-1">
-          <Rating
-          placeholderRating={rating?.number}
-          readonly
-          emptySymbol={<FaRegStar/>}
-          placeholderSymbol={<FaStar className="text-warning"/>}
-          fullSymbol={<FaStar />}
-          ></Rating>
-          <span> {rating?.number}</span>
+        <div className="flex-grow-1 d-flex align-items-center">
+          <Rating 
+            style={{ maxWidth: 150 }} 
+            value={Math.round(rating?.number || 0)} readOnly />
+          <span className="ms-2"> {rating?.number}</span>
         </div>
         <div>
-        <FaEye /> {total_view}
+          <FaEye /> {total_view}
         </div>
       </Card.Footer>
     </Card>
